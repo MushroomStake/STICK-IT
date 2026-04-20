5398f4d27992098d59d3e680b9b8bba6cd1fbb4a
MushroomStake <winterloudrel@gmail.com>
2026-04-20 23:34:29 +0800

    firs file

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+      <h1>Welcome to STICK-IT!</h1>
+      <p>Your Next.js app is running locally.</p>
+    </main>
+  );
+}
